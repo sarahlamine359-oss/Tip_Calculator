1c8d8b080fe07a88e8ded602d3b733c06cbb9b3c
Sarah Lamidi <sarahlamine359@gmail.com>
2026-09-02 14:46:41 +0000

    projetamountTnputRemember

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.tipcalculator
 
 import android.os.Bundle
-import androidx.compose.material3.TextField
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
@@ -19,7 +18,12 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -30,12 +34,13 @@ import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
         setContent {
             TipCalculatorTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize()
                 ) {
                     TipTimeLayout()
                 }
@@ -47,46 +52,55 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipTimeLayout() {
     Column(
-        modifier = Modifier.run {
-            statusBarsPadding()
-                .padding(horizontal = 40.dp)
-                .verticalScroll(rememberScrollState())
-                .safeDrawingPadding()
-        },
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 40.dp)
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = stringResource(R.string.calculate_tip),
             modifier = Modifier
-                .padding(bottom = 16.dp, top = 40.dp)
-                .align(alignment = Alignment.Start)
+                .padding(top = 40.dp, bottom = 16.dp)
+                .align(Alignment.Start)
         )
-        EditNumberField(modifier = Modifier.padding(bottom =
-            32.dp).fillMaxWidth())
+
+        EditNumberField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 32.dp)
+        )
+
         Text(
             text = stringResource(R.string.tip_amount, "$0.00"),
             style = MaterialTheme.typography.displaySmall
         )
+
         Spacer(modifier = Modifier.height(150.dp))
     }
 }
-@Composable
- fun EditNumberField(modifier: Modifier = Modifier) {
-     TextField(
-         value = "",
-         onValueChange = {},
-         modifier = modifier
-       )
-     }
 
+@Composable
+fun EditNumberField(modifier: Modifier = Modifier) {
+    var amountInput by remember { mutableStateOf("") }
+
+    TextField(
+        value = amountInput,
+        onValueChange = { amountInput = it },
+        modifier = modifier
+    )
+}
 
 /**
- * Calculates the tip based on the user input and format the tip amount
+ * Calculates the tip based on the user input and formats the tip amount
  * according to the local currency.
- * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0
+): String {
     val tip = tipPercent / 100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)
 }
